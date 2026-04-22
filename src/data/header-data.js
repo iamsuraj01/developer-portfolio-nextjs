6c4792350564f4f85186be79efd0e45fb868966a
suraj <suraj@rhombuz.io>
2026-04-22 09:04:04 -0400

    refact : added new component for react type

--- src/data/header-data.js
+++ src/data/header-data.js
@@ -1,10 +1,11 @@
 export const headerData = {
   name: "Suraj Gaire",
   title: "Hello! I am",
-  desciption:
-    "Hello! I am ABU SAID, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-  image: "https://i.ibb.co/ZmdNH6x/abu-said.jpg",
+  description:
+    "Hello! I am Suraj Gaire, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+  image:
+    "https://res.cloudinary.com/dtobk2sho/image/upload/v1776837011/1748021898052_tboo40.jpg",
   imagebw: "https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg",
   resumePdf:
-    "https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/",
+    "https://res.cloudinary.com/dtobk2sho/raw/upload/v1776837063/suraj_developer_resume_2026_dymqfb.docx",
 };
