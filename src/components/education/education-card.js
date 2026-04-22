6b5de2422969bed4d36e7578f985da69df0a1d3a
suraj <suraj@rhombuz.io>
2026-04-22 00:56:59 -0400

    refactor: replace react-reveal with framer-motion

--- src/components/education/education-card.js
+++ src/components/education/education-card.js
@@ -1,29 +1,41 @@
-import Image from 'next/image';
-import React, { useContext } from 'react';
-import Fade from 'react-reveal/Fade';
-import eduImgBlack from '../../assets/svg/education/eduImgBlack.svg';
-import eduImgWhite from '../../assets/svg/education/eduImgWhite.svg';
-import { ThemeContext } from '../../contexts/theme-context';
-import styles from '../../styles/education.module.css';
+import { motion } from "framer-motion";
+import Image from "next/image";
+import React, { useContext } from "react";
+import eduImgBlack from "../../assets/svg/education/eduImgBlack.svg";
+import eduImgWhite from "../../assets/svg/education/eduImgWhite.svg";
+import { ThemeContext } from "../../contexts/theme-context";
+import styles from "../../styles/education.module.css";
 
 function EducationCard({ id, institution, course, startYear, endYear }) {
+  const { theme } = useContext(ThemeContext);
 
-    const { theme } = useContext(ThemeContext);
-
-    return (
-        <Fade bottom>
-            <div key={id} className={`${styles.educationCard} bg-[#1E2732]`} >
-                <div className={styles.educardImg} style={{ backgroundColor: theme.primary }}>
-                    <Image src={theme.type === 'light' ? eduImgBlack : eduImgWhite} alt="" />
-                </div>
-                <div className={styles.educationDetails}>
-                    <h6 style={{ color: theme.primary }}>{startYear}-{endYear}</h6>
-                    <h4 style={{ color: theme.tertiary }}>{course}</h4>
-                    <h5 style={{ color: theme.tertiary }}>{institution}</h5>
-                </div>
-            </div>
-        </Fade>
-    )
+  return (
+    <motion.div
+      key={id}
+      className={`${styles.educationCard} bg-[#1E2732]`}
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+    >
+      <div
+        className={styles.educardImg}
+        style={{ backgroundColor: theme.primary }}
+      >
+        <Image
+          src={theme.type === "light" ? eduImgBlack : eduImgWhite}
+          alt=""
+        />
+      </div>
+      <div className={styles.educationDetails}>
+        <h6 style={{ color: theme.primary }}>
+          {startYear}-{endYear}
+        </h6>
+        <h4 style={{ color: theme.tertiary }}>{course}</h4>
+        <h5 style={{ color: theme.tertiary }}>{institution}</h5>
+      </div>
+    </motion.div>
+  );
 }
 
-export default EducationCard
+export default EducationCard;
