d211678d2fde8437d0635e7b67c6abd05bb46ca9
suraj <suraj@rhombuz.io>
2026-04-22 01:08:36 -0400

    fix : config changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+/** @type {import('next').NextConfig} */
+dotenv.config();
 
 const nextConfig = {
   reactStrictMode: true, // enabled react-strict mode
-
+  swcMinify: true,
   images: {
-    domains: [
-      'i.ibb.co',
+    domains: ["i.ibb.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
-
 };
 
 module.exports = nextConfig;
